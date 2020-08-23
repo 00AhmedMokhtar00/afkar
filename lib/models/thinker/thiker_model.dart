7e06c6b41cefd3ee33a33a9cf28c4e229814c7eb
Ahmed Mokhtar <ahmedmokhtar7711@gmail.com>
2020-08-23 04:10:23 +0200

    editing thinkers screen

---
+++ lib/models/thinker/thiker_model.dart
@@ -0,0 +1,29 @@
+
+class ThinkerModel{
+  final String id;
+  final String name;
+  final String email;
+  final String mobile;
+  final String img;
+  final String about;
+  final String address;
+  final String type;
+  final String price;
+  final String token;
+  final String chatFriendID;
+
+  ThinkerModel({
+    this.id,
+    this.name,
+    this.email,
+    this.mobile,
+    this.img,
+    this.about,
+    this.address,
+    this.type,
+    this.price,
+    this.token,
+    this.chatFriendID
+  });
+
+}
